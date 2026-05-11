dad4e8a34af9fe72f0570b97e4158cdbc3d1f4d7
ShahidurRahman2 <imshahidur@gmail.com>
2026-05-11 23:25:09 +0600

    successfully added private route here

---
+++ src/app/components/PrivateRoute.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function PrivateRoute({ children }) {
+
+    const router = useRouter();
+
+    const { data: session, isPending } = authClient.useSession();
+
+    useEffect(() => {
+
+        if (!isPending && !session) {
+            router.push("/login");
+        }
+
+    }, [session, isPending, router]);
+
+    if (isPending) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <span className="loading loading-spinner loading-lg"></span>
+            </div>
+        );
+    }
+
+    if (!session) return null;
+
+    return children;
+}
